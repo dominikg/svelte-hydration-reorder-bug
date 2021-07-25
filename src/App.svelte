b30c7bb957608c01ef23c9c2f41572ecf78206b3
dominikg <dominik.goepel@gmx.de>
2021-07-25 13:26:50 +0200

    simplified reproduction for hydration bug

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { sortItems } from "./lib/sortItems";
+  import Item from "./lib/Item.svelte";
+
+  let sortByName = true;
+  let items = [
+    { id: 1, name: "item 1", value: 3 },
+    { id: 2, name: "item 2", value: 2 },
+    { id: 3, name: "item 3", value: 1 },
+  ];
+
+  $: sortedItems = sortItems(items, sortByName);
+
+  const handleChangeSort = () => {
+    sortByName = !sortByName;
+  };
+</script>
+
+<div>
+  <button on:click={handleChangeSort}> change sort </button>
+
+  <div>
+    sort by name: {sortByName}
+  </div>
+
+  {#each sortedItems as item (item.id)}
+      <Item {item} />
+  {/each}
+</div>
---
+++ src/lib/Item.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let item = {};
+</script>
+
+<div class="item">
+  <!-- wrapping span.name in #if causes the reorder -->
+  {#if item.name}
+    <span class="name">
+      {item.name}
+    </span>
+  {/if}
+
+  <span>something</span>
+
+</div>
+
+<style>
+.name {
+  color: white;
+  background-color: red;
+}
+</style>
